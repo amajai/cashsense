b1e12ebfdf57a389baaffb125e7d39d4273497f8
Abdul <aabdulmajeed.isa@gmail.com>
2024-02-15 23:09:41 +0100

    update frontend

--- frontend/app/__init__.py
+++ frontend/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import os
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
 
 from app import routes
--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, Flask, session, request
 from app import app
 import requests
-
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,7 +11,6 @@ def home():
 def login():
     link_url = url_for('signup')
     link_name = 'Sign Up'
-
     return render_template('login.html', link_url=link_url, link_name=link_name)
 
 @app.route('/signup')
@@ -21,8 +21,20 @@ def signup():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    title = 'Dashboard'
+    return render_template('dashboard.html', title=title)
 
 @app.route('/budgets')
 def budgets():
-    return render_template('budgets.html')
+    title = 'Budgets'
+    return render_template('budgets.html', title=title)
+
+@app.route('/transactions')
+def transactions():
+    title = 'Transactions'
+    return render_template('transactions.html', title=title)
+
+@app.route('/expense')
+def expense():
+    title = 'Expense'
+    return render_template('expense.html', title=title)
